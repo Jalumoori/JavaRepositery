a569fa75bf1b98812ccb69c544add1d3ef847cce
Lavanya <lavanya.avvr@gmail.com>
2023-05-07 16:37:56 -0500

    added test02 file

---
+++ src/srtrings/PrintCharacters.java
@@ -0,0 +1,16 @@
+package srtrings;
+
+public class PrintCharacters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "abcdef";
+		
+		for(char ch : str.toCharArray())
+		{
+			System.out.println(ch);
+		}
+
+	}
+
+}
